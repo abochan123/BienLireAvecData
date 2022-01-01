92d4ac66dc227ce4e996e0a1d28803b9a7d0342f
abochan123 <96906477+abochan123@users.noreply.github.com>
2022-01-01 23:30:49 +0100

    Initial Commit

---
+++ BienLireAvecData/BienLireAvecDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  BienLireAvecDataApp.swift
+//  BienLireAvecData
+//
+//  Created by sawako barberon on 01/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct BienLireAvecDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
